aae6bc755647240b6326bc3a8318eaa5d6ff2e3c
janghyoseog <bestdevhyo1225@gmail.com>
2020-10-19 23:37:36 +0900

    chore: querydsl 관련 라이브러리 추가

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,3 +1,4 @@
+import org.jetbrains.kotlin.gradle.plugin.KotlinSourceSet
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
@@ -7,6 +8,11 @@ plugins {
 	kotlin("plugin.spring") version "1.3.72"
 	kotlin("plugin.jpa") version "1.3.72"
 	kotlin("plugin.allopen") version "1.3.72" // JPA Proxy 객체를 만들기 위해서 반드시 적용해야 함
+	/*  [ querydsl 설정을 위한 추가 ]
+		Java Compiler가 Annotation Processing을 실행하는 과정에서 Kotlin 코드를 인지할 수 없어서
+		JPA Entity class의 쿼리 타입을 생성할 수 없다. 따라서 Kotlin 코드의 Annotation Process을 지원하는 kapt 플로그인을 적용한다.
+	*/
+	kotlin("kapt") version "1.3.72"
 }
 
 // JPA Proxy 객체를 만들기 위해서 반드시 적용해야 함 - 상속 가능하도록 Open 처리
@@ -38,12 +44,25 @@ dependencies {
 	implementation("org.jetbrains.kotlin:kotlin-reflect")
 	implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
 	implementation("com.github.gavlyukovskiy:p6spy-spring-boot-starter:1.5.6")
+
+	/* querydsl 추가 시작 */
+	implementation("com.querydsl:querydsl-jpa")
+	kapt("com.querydsl:querydsl-apt")
+	annotationProcessor("org.springframework.boot:spring-boot-configuration-processor")
+	annotationProcessor(group = "com.querydsl", name = "querydsl-apt", classifier = "jpa")
+	/* querydsl 추가 끝 */
+
 	compileOnly("org.projectlombok:lombok")
 	runtimeOnly("com.h2database:h2")
 	annotationProcessor("org.projectlombok:lombok")
 	testImplementation("org.springframework.boot:spring-boot-starter-test") {
 		exclude(group = "org.junit.vintage", module = "junit-vintage-engine")
 	}
+}
+
+/* querydsl 추가 */
+sourceSets["main"].withConvention(KotlinSourceSet::class){
+	kotlin.srcDir("$buildDir/generated/querydsl")
 }
 
 tasks.withType<Test> {
