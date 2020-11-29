b4b34b2fd912896a4a5ae3f06b5e4d6c5153a7fb
janghyoseog <bestdevhyo1225@gmail.com>
2020-11-29 22:18:48 +0900

    style: Member 엔티티에 Comment 작성

--- src/main/kotlin/com/hyoseok/kotlinjpa/entity/Member.kt
+++ src/main/kotlin/com/hyoseok/kotlinjpa/entity/Member.kt
@@ -31,6 +31,10 @@ class Member : BaseTimeEntity() {
     lateinit var team: Team
         protected set
 
+    /*
+    * 생성자용 static 메소드를 대신하기 위해서 작성함.
+    * -> 생성할 때 뭔가 비즈니스 로직이나 의미있는 생성자 메서드 명이 필요했기 때문에 사용함
+    * */
     companion object {
         @JvmStatic
         fun create(username: String, email: String, team: Team): Member {
@@ -48,12 +52,13 @@ class Member : BaseTimeEntity() {
         }
     }
 
-    // 연관 관계 편의 메소드
+    /* 연관 관계 편의 메소드 */
     fun changeTeam(team: Team) {
         this.team = team
         team.members.add(this)
     }
 
+    /* 비즈니스 로직 */
     fun change(username: String, email: String) {
         this.username = username
         this.email = email
